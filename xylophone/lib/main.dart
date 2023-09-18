52b89a4b9696ba18a5533f0c42212bf7d99ca64e
Gustavoo01 <gustavo.de.souza@hotmail.com>
2023-09-18 16:32:47 -0300

    Finalizando Xylophone

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,22 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-void main() {
-  runApp(const XylophoneApp());
-}
+void main() => runApp(const XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
+
+  void tocar(int soundNumber) async {
+    final player = AudioPlayer();
+    await player.play(AssetSource('note$soundNumber.wav'));
+  }
+
+  Expanded buildKey(int soundNumber, Color buttonColor) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          tocar(soundNumber);
+        },
+        child: Text(""),
+        style: ButtonStyle(
+          backgroundColor:
+              MaterialStateColor.resolveWith((states) => buttonColor),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
-        body: SafeArea(
-          child: Container(child: Text(
-            adjectives.first
-          ),),
+        backgroundColor: Colors.black,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            buildKey(1, Colors.red),
+            buildKey(2, Colors.orange),
+            buildKey(3, Colors.yellowAccent),
+            buildKey(4, Colors.green),
+            buildKey(5, Colors.teal),
+            buildKey(6, Colors.blue),
+            buildKey(7, Colors.deepPurple),
+          ],
         ),
       ),
     );
